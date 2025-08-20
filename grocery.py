02ddcd54b7dd147a3d890b6e7eeee90217ab1ffc
P Maity <patrickmilanohere@proton.me>
2025-08-20 11:53:54 +0530

    Add files via upload

---
+++ grocery.py
@@ -0,0 +1,25 @@
+
+def main():
+    
+    grocery_list = {}
+
+    while True:
+        try:
+
+            item = input().upper()
+
+
+            if item in grocery_list:
+                grocery_list[item] += 1
+            else:
+                grocery_list[item] = 1
+
+        except EOFError:
+
+            print()
+            for item in sorted(grocery_list):
+                print(f"{grocery_list[item]} {item}")
+            break
+
+if __name__ == "__main__":
+    main()
