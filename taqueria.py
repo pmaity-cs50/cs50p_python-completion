b2ccf58ff85c344e70f975d5264248f27607577d
P Maity <patrickmilanohere@proton.me>
2025-08-20 12:00:00 +0530

    Add files via upload

---
+++ taqueria.py
@@ -0,0 +1,75 @@
+
+# def main():
+
+#     menu = {
+#         "Baja Taco": 4.00,
+#         "Burrito": 7.50,
+#         "Bowl": 8.50,
+#         "Nachos": 11.00,
+#         "Quesadilla": 8.50,
+#         "Super Burrito": 8.50,
+#         "Super Quesadilla": 9.50,
+#         "Taco": 3.00,
+#         "Tortilla Salad": 8.00
+#     }
+
+
+#     total = 0.0
+
+
+#     while True:
+#         try:
+#             item = input("Item: ").title()
+#             if item in menu:
+#                 total += menu[item]
+#             print(f"Total: ${total:.2f}")
+#         except EOFError:
+
+#             print("\n")
+#             break
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+def main():
+    # menu = {
+    #     "Baja Taco": 4.00,
+    #     "Burrito": 7.50,
+    #     "Bowl": 8.50,
+    #     "Nachos": 11.00,
+    #     "Quesadilla": 8.50,
+    #     "Super Burrito": 8.50,
+    #     "Super Quesadilla": 9.50,
+    #     "Taco": 3.00,
+    #     "Tortilla Salad": 8.00
+    # }
+
+    menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.75, # <--- Changed price
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+    total = 0.0
+
+    while True:
+        try:
+            item = input("Item: ").title()
+            if item in menu:
+                total += menu[item]
+                print(f"Total: ${total:.2f}")
+        except EOFError:
+            print()
+            break
+
+if __name__ == "__main__":
+    main()
