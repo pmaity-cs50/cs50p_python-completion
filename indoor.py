015b5e3e3363ae163a5120a37047a09076a23f8b
P Maity <patrickmilanohere@proton.me>
2025-08-20 11:54:08 +0530

    Add files via upload

---
+++ indoor.py
@@ -0,0 +1,11 @@
+
+def main():
+    # Prompt the user for input
+    text = input()
+    # Convert the input to lowercase
+    lowercase_text = text.lower()
+    # Print the lowercase text
+    print(lowercase_text)
+
+if __name__ == "__main__":
+    main()
